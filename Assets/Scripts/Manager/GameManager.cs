19f673ed50235f3c253c3f328e7958bec76d0d5d
Bhs1515 <rnjstnqls323@naver.com>
2025-07-28 13:29:01 +0900

    스테이지 씬 추가했습니다.
    쿠키선택창 만드는 중입니다.

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class GameManager : SingletonMonoBehaviour<GameManager>
+{
+    //스테이지 시작하면 스테이지매니저 부르기
+    private void Awake()
+    {
+        DataManager.Instance.LoadAllData();
+    }
+}
